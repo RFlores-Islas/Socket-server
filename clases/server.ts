b7533ce745b880f2463f9b9b030939bb7606a425
RafaelFlo <floresrafa@globalhitss.com>
2022-05-09 15:50:42 -0500

    Seccion 4

--- clases/server.ts
+++ clases/server.ts
@@ -1,14 +1,59 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import  socketIO  from 'socket.io';
+import http from 'http';
+import cors from 'cors';
+import * as socket from '../sockets/socket';
 
 export default class Server {
+
+    private static _instance:Server;
+
     public app:express.Application;
     public port:number;
+    
+    //VIDEO 22
+    public io:socketIO.Server;
+    private httpServer:http.Server;
+    //FIN VIDEO 22
 
-    constructor() {
+    //SINGLETON AYUDA A TENER SOLO UNA CONEXION CON EL SERVIDOR (SE IMPLEMENTA CON PRIVATE)
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+
+         //VIDEO 22
+        this.httpServer = new http.Server(this.app);
+        this.io = require('socket.io')(this.httpServer, {
+            cors: {
+                origin: true,
+                credentials: true
+            },
+        });
+        this.escucharSockets();
+        //FIN VIDEO 22
     }
+
+    //SINGLETON
+    //SI YA EXISTE UNA INSTANCIA, REGRESA ESA INSTANCIA
+    //DE LO CONTRARIO SE CREA UNA NUEVA INSTANCIA
+    public static get instance() {
+        return this._instance || ( this._instance = new this() );
+    }
+
+    //VIDEO 22
+    private escucharSockets() {
+        console.log('Escuchando conexiones');
+        this.io.on('connection', cliente => {
+            console.log('Nuevo cliente conectado');
+        
+            //ENVIAR MENSAJE
+            socket.mensaje(cliente, this.io);
+
+            //DESCONECTAR
+            socket.desconectar(cliente);
+        });
+    }//FIN VIDEO 22
 
     /*start (callback:any) {
         this.app.listen(this.port, callback);
@@ -18,6 +63,6 @@ export default class Server {
     */
 
     start = (callback: (() => void)) => {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
